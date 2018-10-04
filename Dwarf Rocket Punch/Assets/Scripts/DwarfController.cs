89578d8aee894d775eb24b95656bf0b35f49b924
Evan Funnell <funnelle@mymacewan.ca>
2018-10-04 14:16:10 -0600

    Added basic movement to character

--- Dwarf Rocket Punch/Assets/Scripts/DwarfController.cs
+++ Dwarf Rocket Punch/Assets/Scripts/DwarfController.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class DwarfController : MonoBehaviour {
 
+    public bool allowMovement = true;
+    public float maxSpeed = 10f;
+
+    private Rigidbody2D rb2d;
+    private float movementSpeed;
+
 	// Use this for initialization
 	void Start () {
-		
+        rb2d = this.GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (allowMovement) {
+            movementSpeed = Input.GetAxis("Horizontal");
+            rb2d.velocity = new Vector2(movementSpeed * maxSpeed, 0f);
+        }
+    }
 }
