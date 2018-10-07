5a8e4133d90383a4250027a65a3013b90b20836a
Evan Funnell <funnelle@mymacewan.ca>
2018-10-07 02:09:39 -0600

    Added Persistent Scene and Scene Manager, Fixed baked lighting data issue

---
+++ Dwarf Rocket Punch/Assets/Scripts/SceneController/SceneController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+    public CanvasGroup faderCanvasGroup;
+    public float fadeDuration = 1f;
+    public string startingSceneName = "MechanicsTestMap";
+
+    private bool isFading;
+
+	// Use this for initialization
+    private IEnumerator Start () {
+        faderCanvasGroup.alpha = 1f;
+
+        yield return StartCoroutine(LoadSceneAndSetActive(startingSceneName));
+
+        StartCoroutine(Fade(0f));
+	}
+
+    public void FadeAndLoadScene(string sceneName) {
+        if (!isFading) {
+            StartCoroutine(FadeAndSwitchScenes(sceneName));
+        }
+    }
+
+    private IEnumerator FadeAndSwitchScenes(string sceneName) {
+        yield return StartCoroutine(Fade(1f));
+
+        /*
+        if (BeforeSceneUnload != null) {
+            BeforeSceneUnload();
+        }
+        */
+
+        yield return SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+
+        yield return StartCoroutine(LoadSceneAndSetActive(sceneName));
+
+        /*
+        if (AfterSceneLoad != null) {
+            AfterSceneLoad();
+        }
+        */
+        yield return StartCoroutine(Fade(0f));
+    }
+
+    private IEnumerator LoadSceneAndSetActive(string sceneName) {
+        yield return SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+
+        Scene newlyLoadedScene = SceneManager.GetSceneAt(SceneManager.sceneCount - 1);
+        SceneManager.SetActiveScene(newlyLoadedScene);
+    }
+
+    private IEnumerator Fade(float finalAlpha) {
+        isFading = true;
+        faderCanvasGroup.blocksRaycasts = true;
+
+        float fadeSpeed = Mathf.Abs(faderCanvasGroup.alpha - finalAlpha) / fadeDuration;
+
+        while (!Mathf.Approximately(faderCanvasGroup.alpha, finalAlpha)) {
+            faderCanvasGroup.alpha =
+                Mathf.MoveTowards(faderCanvasGroup.alpha, finalAlpha, fadeSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        isFading = false;
+        faderCanvasGroup.blocksRaycasts = false;
+    }
+}
