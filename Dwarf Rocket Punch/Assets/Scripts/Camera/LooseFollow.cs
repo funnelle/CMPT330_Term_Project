685272e94b9f1434e1933999048753e9afdd35a0
Evan Funnell <funnelle@mymacewan.ca>
2018-10-09 11:41:18 -0600

    Added Follow Camera

---
+++ Dwarf Rocket Punch/Assets/Scripts/Camera/LooseFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LooseFollow : MonoBehaviour {
+    //private const float HYSTERESIS = 20f;
+
+    private Transform player;
+
+    public Vector3 offset;
+    public float HYSTERESIS = 10f;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Dwarf").transform;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 startingPosition = player.position + offset;
+        Vector3 where = Vector3.Lerp(transform.position, startingPosition, HYSTERESIS*Time.deltaTime);
+        transform.position = where;
+	}
+}
