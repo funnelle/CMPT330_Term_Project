2fbda3e075dad74cc5aec53f1b72fd2d6b90fce1
Evan Funnell <funnelle@mymacewan.ca>
2018-11-06 11:42:33 -0700

    Added basic structure of an enemy

---
+++ Dwarf Rocket Punch/Assets/Scripts/Enemies/Elf.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(AudioSource))]
+public class Elf : MonoBehaviour {
+    [SerializeField] private bool grounded;
+    [SerializeField] private float groundCheckRadius = 0.1f;
+    [SerializeField] private LayerMask ground;
+    [SerializeField] private float patrolSpeed = 3f;
+    [SerializeField] private float chaseSpeed = 6f;
+
+    private Rigidbody2D rb2d;
+    private BoxCollider2D boxCollider;
+    private AudioSource audioSource;
+    private Transform groundCheck;
+
+    // Use this for initialization
+    void Start() {
+        rb2d = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        audioSource = GetComponent<AudioSource>();
+        groundCheck = this.transform.Find("GroundCheck");
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void FixedUpdate() {
+
+    }
+
+    void Move() {
+
+    }
+
+    void isGrounded() {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, ground);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        
+    }
+}
