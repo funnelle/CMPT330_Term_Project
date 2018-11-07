ecb6c79c607b5e3ed30c5eb4ae9391b4c5bf39d9
horrorvacui <erduct@gmail.com>
2018-11-07 15:44:05 -0700

    Edited dwarf collider, further work on explosion script.
    Dwarf now has pill shaped collider, added a player layer so the raycast from the punch will ignore the dwarf himself. Also updated parts of the script to find the gauntlet from the main dwarf himself.

--- Dwarf Rocket Punch/Assets/Scripts/Player/DwarfRocketFire.cs
+++ Dwarf Rocket Punch/Assets/Scripts/Player/DwarfRocketFire.cs
@@ -18,8 +18,22 @@ public class DwarfRocketFire : MonoBehaviour
     private float timeSinceFire;
     private RaycastHit2D hitCheck;
     private Ray2D dwarfPunch;
+    private LayerMask playerLayer = ~1 << 9;
 
-    // Update is called once per frame
+    private Transform gauntletLocation;
+    /// <summary>
+    /// Find our gauntlet game objects location for use in our punch.
+    /// </summary>
+    /// EW 2018-11-07
+    private void Start()
+    {
+        gauntletLocation = GameObject.Find("/Dwarf/MainAnimationRig/Torso/Arms/Gauntlet").GetComponent<Transform>();
+    }
+
+    /// <summary>
+    /// Check if we're allowed to fire again, and if so, call the punch function.
+    /// </summary>
+    /// EW 2018-11-07
     void Update()
     {
 
@@ -29,14 +43,20 @@ public class DwarfRocketFire : MonoBehaviour
             Punch();
         }
     }
+
+    /// <summary>
+    /// Fire a raycast in the direction of the dwarfs gauntlet, and create a circle (the explosion), and from there
+    /// apply our explosion to each rigidbody that the circle has made contact with.
+    /// </summary>
+    /// EW 2018-11-07
     void Punch()
     {
         timeSinceFire = 0f;
         //reset our punch time
-        dwarfPunch.origin = transform.position;
-        dwarfPunch.direction = transform.forward;
+        dwarfPunch.origin = gauntletLocation.position;
+        dwarfPunch.direction = gauntletLocation.forward;
         //Get our location
-        hitCheck = Physics2D.Raycast(dwarfPunch.origin, dwarfPunch.direction, punchRange);
+        hitCheck = Physics2D.Raycast(dwarfPunch.origin, dwarfPunch.direction, punchRange, playerLayer);
         if (hitCheck.collider != null)
         {
             print("We've hit something");
@@ -55,12 +75,14 @@ public class DwarfRocketFire : MonoBehaviour
     }
 
     /// <summary>
-    /// This function calculates the direction of force to be applied based on the explosion 
+    /// This function calculates the direction of force to be applied based on the explosions parameters. 
+    /// It will send whichever rigidbody is supplied into the calculated direction at the specified force.
     /// </summary>
-    /// <param name="expVictim"></param>
-    /// <param name="explosionForce"></param>
-    /// <param name="explosionPos"></param>
-    /// <param name="explosionRadius"></param>
+    /// <param name="expVictim">The Rigidbody 2D we wish to apply force to.</param>
+    /// <param name="explosionForce">Desired strength of the explosion (a float).</param>
+    /// <param name="explosionPos">The origin of the explosion (A vector2 location.)</param>
+    /// <param name="explosionRadius">The width of the explosion. (A float.)</param>
+    /// EW 2018-11-07
     void DwarfExplode(Rigidbody2D expVictim, float explosionForce, Vector2 explosionPos, float explosionRadius)
     {
         Vector2 ExpDir = (Vector2)expVictim.transform.position - explosionPos;
