ac2c15e672eb57dfb10662e292f1713096361d5e
mccormicke3 <eamonn6251@gmail.com>
2018-11-07 09:43:59 -0700

    Flip function in Dwarf Controller
    Got the flip function working, the last line of the function allows the arm to properly track the mouse when the character changes direction (otherwise the arm points directly away from the mouse position, as apposed to toward it).

--- Dwarf Rocket Punch/Assets/Scripts/Player/DwarfController.cs
+++ Dwarf Rocket Punch/Assets/Scripts/Player/DwarfController.cs
@@ -29,9 +29,10 @@ public class DwarfController : MonoBehaviour {
     public LayerMask ground;
 
     private Rigidbody2D rb2d;
-    private float movementSpeed;
     private Transform groundCheck;
-    //private Animator animator;
+    private Animator mainAnimator;
+    private bool facingRight;
+    private float movementSpeed;
 
     /// <summary>
     /// Initialize variables at game start
@@ -42,7 +43,7 @@ public class DwarfController : MonoBehaviour {
     void Start() {
         rb2d = this.GetComponent<Rigidbody2D>();
         groundCheck = GameObject.Find("/Dwarf/GroundCheck").GetComponent<Transform>();
-        //animator = GameObejct.Find("/Dwarf/MainAnimationRig").GetComponent<Animator>();
+        mainAnimator = GameObject.Find("/Dwarf/MainAnimationRig").GetComponent<Animator>();
     }
 
     /// <summary>
@@ -55,7 +56,6 @@ public class DwarfController : MonoBehaviour {
         if (allowMovement) {
             movementSpeed = Input.GetAxis("Horizontal");
             rb2d.velocity = new Vector2(movementSpeed * maxSpeed, rb2d.velocity.y);
-            // set animator.isRunning to true
         }
     }
 
@@ -68,6 +68,20 @@ public class DwarfController : MonoBehaviour {
     void FixedUpdate() {
         //create a sphere that checks if we are on ground
         onGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, ground);
+
+        if (Mathf.Abs(rb2d.velocity.x) > 0.01f) {
+            mainAnimator.SetBool("isRunning", true);
+        }
+        else {
+            mainAnimator.SetBool("isRunning", false);
+        }
+
+        if (rb2d.velocity.x < 0 && !facingRight) {
+            Flip();
+        }
+        else if (rb2d.velocity.x > 0 && facingRight) {
+            Flip();
+        }
     }
 
     //Debug function to test OnGround check
@@ -76,5 +90,20 @@ public class DwarfController : MonoBehaviour {
         Gizmos.color = Color.yellow;
         Gizmos.DrawSphere(groundCheck.position, groundCheckRadius);
     }
-}
 
+    /// <summary>
+    /// FLips the render of the character when the velocity parity flips (the player turns around)
+    /// </summary>
+    /// 
+    /// 2018-11-7 EPM       Added flip code
+    /// 
+    void Flip() {
+        facingRight = !facingRight;
+        //flips parity of x-axis render, flipping the character around
+        Vector3 mainScale = transform.localScale;
+        mainScale.x *= -1;
+        transform.localScale = mainScale;
+        //flips parity of mouse track vector so that shoulder does not track mouse when player turns
+        TrackMouse.directionModifier *= -1;
+    }
+}
