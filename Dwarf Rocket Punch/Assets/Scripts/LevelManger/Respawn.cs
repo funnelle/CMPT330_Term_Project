05fee361f5a6a5f55cb309054eb6cdd326c9641e
Eric Stratechuk <ericstratechuk@gmail.com>
2018-11-23 12:22:57 -0700

    Added Respawn System

---
+++ Dwarf Rocket Punch/Assets/Scripts/LevelManger/Respawn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.transform.position = GameObject.FindWithTag("ReSpawnArea").transform.position;
+        }
+    }
+}
