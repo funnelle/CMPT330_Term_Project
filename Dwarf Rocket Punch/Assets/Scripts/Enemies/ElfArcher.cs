9d0b1ffe474f2e4f26086c9349760f4430c03e95
Evan Funnell <funnelle@mymacewan.ca>
2018-12-02 12:39:04 -0700

    Added raycast detection for AI, Removed unused components in Dwarf Prefab

--- Dwarf Rocket Punch/Assets/Scripts/Enemies/ElfArcher.cs
+++ Dwarf Rocket Punch/Assets/Scripts/Enemies/ElfArcher.cs
@@ -7,22 +7,35 @@ public class ElfArcher : Elf {
 
     private float detectionAngleInRads;
     private float detectionCos;
-    private Transform playerPosition;
     private float elfPlayerDot;
+    private Vector2 rayDirection;
+
+    public Transform playerPosition;
 
     protected override void Start() {
         base.Start();
-        playerPosition = transform.Find("Dwarf");
         detectionAngleInRads = detectionAngle * Mathf.Deg2Rad;
         detectionCos = Mathf.Cos(detectionAngleInRads);
     }
 
     protected override void Update() {
         base.Update();
+        rayDirection = playerPosition.position - transform.position;
+       
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, rayDirection);
+        Debug.DrawRay(transform.position, rayDirection, Color.red);
         elfPlayerDot = Vector2.Dot(transform.position, playerPosition.position);
+
         if (detectionCos > elfPlayerDot) {
             Debug.Log("I see you");
-            state = State.ATTACKING;
+            Debug.Log(hit.collider.name);
+            Debug.Log(playerPosition.name);
+            if (hit.collider.name != playerPosition.parent.name) {
+                Debug.Log("Why you behind a wall, boo you suck");
+            }
+            else {
+                state = State.ATTACKING;
+            }
         }
 
         if (state == State.ATTACKING) {
@@ -30,6 +43,6 @@ public class ElfArcher : Elf {
         }
     }
     private void AttackMode() {
-        Debug.Log("I'm attacking");
+        Debug.Log("I'm attacking, ARRGGGG");
     }
 }
