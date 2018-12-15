b626c1dc51e3cabdad2cf4dd684b3c14d911fd63
horrorvacui <erduct@gmail.com>
2018-12-14 20:46:16 -0700

    Dwarf Respawn from Arrows, Commenting on Code

--- Dwarf Rocket Punch/Assets/Scripts/LevelManger/Respawn.cs
+++ Dwarf Rocket Punch/Assets/Scripts/LevelManger/Respawn.cs
@@ -2,18 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// When the player contacts these areas, respawns them. "Hazard areas."
+/// </summary>
+/// Field       Summary
+/// 
+/// deathCameraTime  The amount of time we wait for the camera to find the dwarf again.
+/// 
+/// Author: Eric Stratechuk - Coding
+///         Eric Walker - Commenting
 public class Respawn : MonoBehaviour {
     public float deathCameraTime;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    
+    /// <summary>
+    /// When the player enters this collider, respawn them. 
+    /// </summary>
+    /// <param name="collision">The collision we are detecting.</param>
+    /// ES 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
@@ -27,6 +32,13 @@ public class Respawn : MonoBehaviour {
         }
     }
 
+    /// <summary>
+    /// Respawn the player by resetting the players transform, and make the camera wait for a moment.
+    /// </summary>
+    /// <param name="collision">The player collision.</param>
+    /// <param name="deathCameraTime">The amount of time waiting before pulling the camera.</param>
+    /// <returns>The time returned in game seconds before finishing the coroutine.</returns>
+    /// ES
     public IEnumerator StopDwarfMovement(Collider2D collision, float deathCameraTime)
     {
         //yield return new WaitForSeconds(deathCameraTime);
